40e0b9958bb89066835a6f6505d3eec4214bcdf8
RutvikMakvana4 <rutvikmakvana432@gmail.com>
2024-11-15 12:04:10 +0530

    feat: set view engine template

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
