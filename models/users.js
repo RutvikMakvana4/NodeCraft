20aa46c04a8d9fe1f85da861c3849eb564c8ef7a
RutvikMakvana4 <rutvikmakvana432@gmail.com>
2024-11-15 12:15:05 +0530

    feat: setup user services

---
+++ models/users.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      index: {
+        unique: true,
+      },
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    joinedAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
